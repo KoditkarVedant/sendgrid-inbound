74d1ab9276ebee579d94a0a1b8adca0d3e06b070
KoditkarVedant <vedkoditkar@gmail.com>
2018-10-14 00:35:32 -0700

    Add more UnitTests for InboudWebhookParser

--- tests/Inbound.Tests/Parsers/InboundWebhookParserTests.cs
+++ tests/Inbound.Tests/Parsers/InboundWebhookParserTests.cs
@@ -70,5 +70,52 @@ namespace Inbound.Tests.Parsers
             
             inboundEmail.Spf.ShouldBe("pass");
         }
+
+        [Fact]
+        public async void RawPayloadWithAttachments()
+        {
+            Stream stream = new MemoryStream();
+            await File.OpenRead("sample_data/raw_data_with_attachments.txt").CopyToAsync(stream);
+            stream.Position = 0;
+
+            var parser = new InboundWebhookParser(stream);
+
+            InboundEmail inboundEmail = parser.Parse();
+
+            inboundEmail.ShouldNotBeNull();
+
+            inboundEmail.Dkim.ShouldBe("{@sendgrid.com : pass}");
+
+            var rawEmailTestData = await File.ReadAllTextAsync("sample_data/raw_email_with_attachments.txt");
+            inboundEmail.RawEmail.Trim().ShouldBe(rawEmailTestData);
+
+            inboundEmail.To[0].Email.ShouldBe("inbound@inbound.example.com");
+            inboundEmail.To[0].Name.ShouldBe(string.Empty);
+
+            inboundEmail.Cc.Length.ShouldBe(0);
+
+            inboundEmail.From.Email.ShouldBe("test@example.com");
+            inboundEmail.From.Name.ShouldBe("Example User");
+
+            inboundEmail.SenderIp.ShouldBe("0.0.0.0");
+
+            inboundEmail.SpamReport.ShouldBeNull();
+
+            inboundEmail.Envelope.From.ShouldBe("test@example.com");
+            inboundEmail.Envelope.To.Length.ShouldBe(1);
+            inboundEmail.Envelope.To.ShouldContain("inbound@inbound.example.com");
+
+            inboundEmail.Subject.ShouldBe("Raw Payload");
+
+            inboundEmail.SpamScore.ShouldBeNull();
+
+            inboundEmail.Charsets.Except(new[] {
+                new KeyValuePair<string, Encoding>("to", Encoding.UTF8),
+                new KeyValuePair<string, Encoding>("subject", Encoding.UTF8),
+                new KeyValuePair<string, Encoding>("from", Encoding.UTF8)
+            }).Count().ShouldBe(0);
+
+            inboundEmail.Spf.ShouldBe("pass");
+        }
     }
 }
